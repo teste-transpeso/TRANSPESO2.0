14566305703f20e4d8d586d3cd35d22d087eba30
teste-transpeso <107429593+teste-transpeso@users.noreply.github.com>
2022-08-21 23:08:22 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+function ganhaCor(id){
+    let elemento = document.getElementById(id)
+    elemento.style.fontSize = '10pt';
+    elemento.style.transition = '0.3s';
+    elemento.style.filter = "grayscale(0%)"
+    elemento.style.width = "300px"
+     
+     
+     console.log(elemento)
+}
+function perdeCor(id){
+    let elemento = document.getElementById(id)
+    elemento.style.fontSize = '120%';
+     elemento.style.filter = "grayscale(100%)"
+     elemento.style.width = "270px"
+     console.log(elemento)
+}
+
+
+
+function GanhaNome(id){
+    let h2 = document.getElementById(id)
+    h2.style.transition = ("0.5s")
+    h2.style.marginTop = ("0px")
+
+    let nome =  document.getElementsByClassName('titu-zap')
+    nome[id].style.transition = ("0.5s")
+    nome[id].style.marginTop = ("9px")
+    nome[id].style.visibility = ("visible")
+}
+
+function PerdeNome(id){
+    let h2 = document.getElementById(id)
+    h2.style.transition = ("0.5s")
+    h2.style.marginTop = ("20px")
+
+    let nome =  document.getElementsByClassName('titu-zap')
+    console.log(nome[id]);
+    nome[id].style.transition = ("0.5s")
+    nome[id].style.marginTop = ("0px")
+    nome[id].style.visibility = ("hidden")
+}
+
